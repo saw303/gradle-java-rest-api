d08e1cf5a369e76a9c4b2d71f6d3a4d473baf8a3
Silvio Wangler <silvio.wangler@gmail.com>
2018-01-29 10:14:31 +0100

    Corrected license header

--- src/main/groovy/ch/silviowangler/gradle/restapi/builder/AbstractResourceBuilder.java
+++ src/main/groovy/ch/silviowangler/gradle/restapi/builder/AbstractResourceBuilder.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * <p>
+ *
  * Copyright (c) 2016 - 2018 Silvio Wangler (silvio.wangler@gmail.com)
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,10 +23,10 @@
  */
 package ch.silviowangler.gradle.restapi.builder;
 
-import ch.silviowangler.gradle.restapi.PluginTypes;
 import ch.silviowangler.gradle.restapi.GenerateRestApiTask;
 import ch.silviowangler.gradle.restapi.GeneratorUtil;
 import ch.silviowangler.gradle.restapi.LinkParser;
+import ch.silviowangler.gradle.restapi.PluginTypes;
 import ch.silviowangler.rest.contract.model.v1.*;
 import com.google.common.base.CaseFormat;
 import com.squareup.javapoet.*;
