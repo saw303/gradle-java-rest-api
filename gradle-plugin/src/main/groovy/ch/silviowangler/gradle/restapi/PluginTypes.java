ebf47130e1824c30dca77f2ec84c704cddc490e1
Urs Wiss <urs@3ap.ch>
2018-08-16 16:27:02 +0200

    remove not used types

--- gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/PluginTypes.java
+++ gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/PluginTypes.java
@@ -1,25 +1,25 @@
-/**
- * MIT License
- * <p>
- * Copyright (c) 2016 - 2018 Silvio Wangler (silvio.wangler@gmail.com)
- * <p>
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * <p>
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * <p>
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+/*
+  MIT License
+  <p>
+  Copyright (c) 2016 - 2018 Silvio Wangler (silvio.wangler@gmail.com)
+  <p>
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  <p>
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  <p>
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
  */
 package ch.silviowangler.gradle.restapi;
 
@@ -31,9 +31,6 @@ import com.squareup.javapoet.ClassName;
 public enum PluginTypes {
 
 	RESTAPI_IDTYPE(ClassName.get("ch.silviowangler.rest.types", "IdType")),
-	RESTAPI_FILTERMODEL(ClassName.get("ch.silviowangler.rest.model", "FilterModel")),
-	RESTAPI_JWT_ANNOTATION(ClassName.get("ch.silviowangler.rest.security", "SecurityEnabled")),
-	RESTAPI_CACHING_ANNOTATION(ClassName.get("ch.silviowangler.rest.cache", "CacheSetting")),
 	RESTAPI_RESOURCE_MODEL(ClassName.get("ch.silviowangler.rest.model", "ResourceModel")),
 	JAX_RS_RESPONSE(ClassName.get("javax.ws.rs.core", "Response")),
 	JAX_RS_OPTIONS_VERB(ClassName.get("javax.ws.rs", "OPTIONS")),
@@ -42,12 +39,9 @@ public enum PluginTypes {
 	JAX_RS_PUT_VERB(ClassName.get("javax.ws.rs", "PUT")),
 	JAX_RS_DELETE_VERB(ClassName.get("javax.ws.rs", "DELETE")),
 	JAX_RS_PRODUCES(ClassName.get("javax.ws.rs", "Produces")),
-	JAX_RS_CONSUMES(ClassName.get("javax.ws.rs", "Consumes")),
 	JAX_RS_PATH(ClassName.get("javax.ws.rs", "Path")),
-	JAX_RS_FORM_PARAM(ClassName.get("javax.ws.rs", "FormParam")),
 	JAX_RS_PATH_PARAM(ClassName.get("javax.ws.rs", "PathParam")),
 	JAX_RS_QUERY_PARAM(ClassName.get("javax.ws.rs", "QueryParam")),
-	JAX_RS_CONTEXT(ClassName.get("javax.ws.rs.core", "Context")),
 	JAVAX_VALIDATION_SIZE(ClassName.get("javax.validation.constraints", "Size")),
 	JAVAX_VALIDATION_DECIMAL_MIN(ClassName.get("javax.validation.constraints", "DecimalMin")),
 	JAVAX_VALIDATION_DECIMAL_MAX(ClassName.get("javax.validation.constraints", "DecimalMax")),
