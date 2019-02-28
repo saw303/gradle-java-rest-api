bfbe5bece986830418b4a78cfcc98fc32d3ea893
Silvio Wangler <silvio.wangler@gmail.com>
2019-02-28 10:29:29 +0100

    1.3.4 - Fix dependency mess :(

--- gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/RestApiPlugin.groovy
+++ gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/RestApiPlugin.groovy
@@ -74,7 +74,7 @@ class RestApiPlugin implements Plugin<Project> {
         }
 
         final String springVersion = '5.1.3.RELEASE'
-        final String pluginVersion = "1.3.0"
+        final String pluginVersion = "1.3.4"
 
         project.afterEvaluate {
             project.dependencies {
