9af3d30f19e021e926ebe6016feba0f14a79a4d8
Silvio Wangler <silvio.wangler@gmail.com>
2019-03-28 15:23:37 +0100

    1.4.4 - Generator uses DateFormat & DateTimeFormat annotations on query string parameters
    Signed-off-by: Silvio Wangler <silvio.wangler@gmail.com>

--- gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/RestApiPlugin.groovy
+++ gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/RestApiPlugin.groovy
@@ -74,7 +74,7 @@ class RestApiPlugin implements Plugin<Project> {
         }
 
         final String springVersion = '5.1.3.RELEASE'
-        final String pluginVersion = "1.4.3"
+        final String pluginVersion = "1.4.4"
 
         project.afterEvaluate {
             project.dependencies {
