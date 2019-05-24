005a487b76fd83ab357446f8c7b8cbf5c267add5
Silvio Wangler <silvio.wangler@gmail.com>
2019-05-24 14:58:22 +0200

    Upgraded deps
    Signed-off-by: Silvio Wangler <silvio.wangler@gmail.com>

--- gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/RestApiPlugin.groovy
+++ gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/RestApiPlugin.groovy
@@ -84,7 +84,7 @@ class RestApiPlugin implements Plugin<Project> {
 				api "ch.silviowangler.rest:rest-model:${pluginVersion}"
 				api "javax.money:money-api:1.0.3"
 				api "javax.validation:validation-api:2.0.1.Final"
-				api "com.googlecode.libphonenumber:libphonenumber:8.10.11"
+				api "com.googlecode.libphonenumber:libphonenumber:${libPhoneNumberVersion}"
 
 				if (extension.generationMode != GenerationMode.API) {
 
