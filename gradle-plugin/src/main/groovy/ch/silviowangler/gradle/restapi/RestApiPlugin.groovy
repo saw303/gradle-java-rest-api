26db818cc032dd042e75527368a4e5c76e11379e
Silvio Wangler <silvio.wangler@gmail.com>
2019-07-29 19:22:45 +0200

    Set 2.1.0-SNAPSHOT
    Signed-off-by: Silvio Wangler <silvio.wangler@gmail.com>

--- gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/RestApiPlugin.groovy
+++ gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/RestApiPlugin.groovy
@@ -74,7 +74,7 @@ class RestApiPlugin implements Plugin<Project> {
 		}
 
 		final String springVersion = "5.1.3.RELEASE"
-		final String pluginVersion = "2.1.0"
+		final String pluginVersion = "2.1.0-SNAPSHOT"
 		final String libPhoneNumberVersion = "8.10.12"
 
 		project.afterEvaluate {
