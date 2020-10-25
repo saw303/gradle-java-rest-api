2164744ba6c8805bf83c132502700fa2ed5bed1c
Silvio Wangler <71073+saw303@users.noreply.github.com>
2020-10-25 11:21:22 +0100

    2.2.16 - Fix Header comment problem
    Signed-off-by: Silvio Wangler <71073+saw303@users.noreply.github.com>

--- gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/RestApiPlugin.groovy
+++ gradle-plugin/src/main/groovy/ch/silviowangler/gradle/restapi/RestApiPlugin.groovy
@@ -102,7 +102,7 @@ class RestApiPlugin implements Plugin<Project> {
 		}
 
 		final String springVersion = "5.2.4.RELEASE"
-		final String pluginVersion = "2.2.15"
+		final String pluginVersion = "2.2.16"
 		final String libPhoneNumberVersion = "8.11.5"
 
 		project.afterEvaluate {
