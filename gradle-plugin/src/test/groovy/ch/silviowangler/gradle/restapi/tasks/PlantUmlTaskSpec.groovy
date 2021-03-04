59d9f882d30dda67014f02732348cf047a7341fc
Silvio Wangler <71073+saw303@users.noreply.github.com>
2021-03-04 17:50:38 +0100

    Improve test
    Signed-off-by: Silvio Wangler <71073+saw303@users.noreply.github.com>

--- gradle-plugin/src/test/groovy/ch/silviowangler/gradle/restapi/tasks/PlantUmlTaskSpec.groovy
+++ gradle-plugin/src/test/groovy/ch/silviowangler/gradle/restapi/tasks/PlantUmlTaskSpec.groovy
@@ -12,19 +12,18 @@ class PlantUmlTaskSpec extends Specification {
   static String resourceName = "resource"
 
   @Unroll
-  void "CommonPrefix"() {
+  void "The common prefix of '#a' and '#b' is '#prefix'"() {
 
     expect:
     PlantUmlTask.commonPrefix(a, b) == prefix
 
     where:
-    a            | b                || prefix
-    ""           | ""               || ""
-    null         | ""               || ""
-    null         | null             || ""
-    "HEy"        | "Hey"            || "H"
-    "Ciao Bella" | "Ciao Bella Ciao" | "Ciao Bella"
-
+    a            | b                 || prefix
+    ""           | ""                || ""
+    null         | ""                || ""
+    null         | null              || ""
+    "HEy"        | "Hey"             || "H"
+    "Ciao Bella" | "Ciao Bella Ciao" || "Ciao Bella"
   }
 
   @Unroll
@@ -33,18 +32,16 @@ class PlantUmlTaskSpec extends Specification {
     given:
     SubResource subResource = new SubResource(name: resourceName, href: subresource)
 
-
     when:
     ResourceContractContainer foundResource = PlantUmlTask.findSubResourceContract(resources, subResource)
 
     then:
-    foundResource.resourceContract.general.description == expectedResource.toString()
+    foundResource.resourceContract.general.description == expectedResource as String
 
     where:
     resources                                                              | subresource                    || expectedResource
     [c(1, "/v1/some/name/here"), c(2, "/v1/some/other/name")]              | "/v1/some/name"                || 1
     [c(1, "/v1/:pin/name/:name/text"), c(2, "/v1/:pin/name/:name/number")] | "/v1/{:pin}/name/{:NAME}/text" || 1
-
   }
 
   static ResourceContractContainer c(int id, String route) {
