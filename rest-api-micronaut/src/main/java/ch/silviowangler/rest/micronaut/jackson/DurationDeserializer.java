555b3183baba00887b6cdf94a9ea913434db55eb
Silvio Wangler <71073+saw303@users.noreply.github.com>
2022-03-21 13:53:04 +0100

    Added Jackson de- & serializers for Micronaut and Duration

---
+++ rest-api-micronaut/src/main/java/ch/silviowangler/rest/micronaut/jackson/DurationDeserializer.java
@@ -0,0 +1,46 @@
+/*
+ * MIT License
+ * <p>
+ * Copyright (c) 2016 - 2022 Silvio Wangler (silvio.wangler@gmail.com)
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package ch.silviowangler.rest.micronaut.jackson;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import java.io.IOException;
+import java.time.Duration;
+import javax.inject.Singleton;
+
+@Singleton
+public class DurationDeserializer extends StdDeserializer<Duration> {
+
+  protected DurationDeserializer() {
+    super(Duration.class);
+  }
+
+  @Override
+  public Duration deserialize(JsonParser p, DeserializationContext ctxt)
+      throws IOException, JsonProcessingException {
+    return Duration.parse(p.getText());
+  }
+}
